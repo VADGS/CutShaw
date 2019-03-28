7b9ede114e42432104a666214879ae1ed30b04af
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2019-03-28 08:06:26 -0400

    initiate assembly if none exists

--- lib/run_fastani.py
+++ lib/run_fastani.py
@@ -6,12 +6,13 @@
 import os
 import sys
 import shutil
+import csv
 import argparse
 import re
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
 from CutShaw.core import fileparser
 from CutShaw.core import calldocker
-
+from CutShaw.lib import run_spades
 
 class FastANI:
     # class object to contain fastq file information
@@ -61,6 +62,10 @@ class FastANI:
                 self.path = self.output_dir
             assembly = "/spades_output/%s/contigs.fasta" % id
 
+            if not os.path.isfile(assembly):
+                spades_obj = run_spades.Spades(path=self.path, output_dir=self.output_dir)
+                spades_obj.spades()
+
             # create paths for data
             mounting = {self.path:'/datain',fastani_out_dir:'/dataout', self.db: '/db'}
             ref_list = "/reference_list.txt"
@@ -78,28 +83,28 @@ class FastANI:
                 calldocker.call("staphb/fastani",command,'/dataout',mounting)
 
             with open("%s/%s"%(fastani_out_dir, fastani_result)) as file:
-                #rd = csv.reader(file, delimiter="\t", quotechar='"')
-                for line in file:
-                    parts = line.split()
-                    predicted_taxon = ""
-                    reference_genome = str(os.path.basename(parts[1]))
-                    if "SAP18-0432" in reference_genome:
-                        predicted_taxon = "Salmonella enterica subsp. enterica serover Enteritidis"
-                    elif "SAP18-H9654" in reference_genome:
-                        predicted_taxon = "Salmonella enterica subsp. enterica serover Enteritidis"
-                    elif "SAP18-6199" in reference_genome:
-                        predicted_taxon = "Salmonella enterica subsp. enterica serover Typhimurium"
-                    elif "SAP18-8729" in reference_genome:
-                        predicted_taxon = "Salmonella enterica subsp. enterica serover Newport"
-                    elif "LMP18-H2446" in reference_genome:
-                        predicted_taxon = "Listeria monocytogenes"
-                    elif "LMP18-H8393" in reference_genome:
-                        predicted_taxon = "Listeria monocytogenes"
-                    else:
-                        raise ValueError("Sample %s not identified as a 2018 PT isolate"%id)
+                tsv_reader = csv.reader(file, delimiter="\t", quotechar='"')
+                predicted_taxon = ""
+                reference_genome = str(os.path.basename(next(tsv_reader)[1]))
+                if "SAP18-0432" in reference_genome:
+                    predicted_taxon = "Salmonella enterica subsp. enterica serover Enteritidis"
+                elif "SAP18-H9654" in reference_genome:
+                    predicted_taxon = "Salmonella enterica subsp. enterica serover Enteritidis"
+                elif "SAP18-6199" in reference_genome:
+                    predicted_taxon = "Salmonella enterica subsp. enterica serover Typhimurium"
+                elif "SAP18-8729" in reference_genome:
+                    predicted_taxon = "Salmonella enterica subsp. enterica serover Newport"
+                elif "LMP18-H2446" in reference_genome:
+                    predicted_taxon = "Listeria monocytogenes"
+                elif "LMP18-H8393" in reference_genome:
+                    predicted_taxon = "Listeria monocytogenes"
+                else:
+                    raise ValueError("Sample %s not identified as a 2018 PT isolate"%id)
 
             taxons[id] = predicted_taxon
             reference_genomes[id] = reference_genome
+            print("Isolate: %s " %id)
+            print("reference genome: %s"%reference_genome)
 
            # print("Sample: " + id + " FastANI Hit: " + str(predicted_taxon))
 
@@ -118,7 +123,6 @@ if __name__ == '__main__':
 
     path = os.path.abspath(args.input)
     output_dir = args.o
-    species = args.species
 
     if not output_dir:
         output_dir = os.getcwd()
